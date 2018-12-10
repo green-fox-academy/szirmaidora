93e9706a59e724d2ef775d48639af5a063d5bd6a
Szirmai Dóra <szirmaidora@gmail.com>
2018-12-10 17:00:07 +0100

    HighCard

---
+++ Orientation/week-07/Dojo TDD/src/Card.java
@@ -0,0 +1,11 @@
+public class Card {
+    String value;
+    String suit;
+
+    public Card(String value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+
+}
--- Orientation/week-07/Dojo TDD/src/Poker.java
+++ Orientation/week-07/Dojo TDD/src/Poker.java
@@ -1,4 +1,8 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Poker {
     public static void main(String[] args) {
@@ -10,13 +14,44 @@ public class Poker {
         String black = "Black wins!";
         String winner;
 
-        if (Arrays.deepEquals(new String[][]{{"2", "H"}, {"3", "D"}, {"5", "S"}, {"9", "C"}, {"K", "D"}}, hand1)
-                && Arrays.deepEquals(new String[][]{{"2", "C"}, {"3", "H"}, {"4", "S"}, {"8", "C"}, {"A", "H"}}, hand2)) {
-            winner = white;
-        } else {
-            winner = black;
+        for (int i = 0; i < hand1.length; i++) {
+            for (int j = 0; j < hand2.length ; j++) {
+                if (hand1[i][0].equals("A") || hand2[j][0].equals("A")) {
+                    if (hand1[i][0].equals("A")) {
+                        return black;
+                    }
+                    return white;
+                } else if (hand1[i][0].equals("K") || hand2[j][0].equals("K")) {
+                    if (hand1[i][0].equals("K")) {
+                        return black;
+                    }
+                    return white;
+                } else if (hand1[i][0].equals("Q") || hand2[j][0].equals("Q")) {
+                    if (hand1[i][0].equals("Q")) {
+                        return black;
+                    }
+                    return white;
+                } else if (hand1[i][0].equals("J") || hand2[j][0].equals("J")) {
+                    if (hand1[i][0].equals("J")) {
+                        return black;
+                    }
+                    return white;
+                } else if (Integer.parseInt(hand1[i][0]) > Integer.parseInt(hand2[j][0])) {
+                    return black;
+                }
+            }
         }
+        return white;
+    }
 
-        return winner;
+    public static String highCard(List<Card> hand) {
+        List<String> values = hand.stream().map(card -> card.value).collect(Collectors.toList());
+        String[] letters = {"A", "K", "Q", "J"};
+        for (int i = 0; i < letters.length; i++) {
+            if (values.contains(letters[i])) {
+                return letters[i];
+            }
+        }
+     return null;
     }
 }
