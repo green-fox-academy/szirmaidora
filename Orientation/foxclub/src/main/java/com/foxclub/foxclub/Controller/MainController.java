37c6a5096d27db4c79134254e162ff617219181d
Szirmai Dóra <szirmaidora@gmail.com>
2019-01-04 20:51:43 +0100

    nemukodo foxclub :(

--- Orientation/foxclub/src/main/java/com/foxclub/foxclub/Controller/MainController.java
+++ Orientation/foxclub/src/main/java/com/foxclub/foxclub/Controller/MainController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -18,8 +20,12 @@ public class MainController {
         this.foxService = foxService;
     }
 
-    @GetMapping(path = "/")
-    public String index() {
+    @GetMapping({"/"})
+    public String index(@RequestParam(name = "name", required = false) String name, Model model) {
+        if(name == null){
+            return "Login";
+        }
+        model.addAttribute("fox", foxService.findFox(name));
         return "index";
     }
 
@@ -30,13 +36,18 @@ public class MainController {
     }
 
     @PostMapping("/login")
-    public String loging(@RequestParam(name = "name", required = true) String name, Model model) {
+    public String loging(@RequestParam(name = "name", required = true) String name, RedirectAttributes redirectAttributes) {
         foxService.saveFox(new Fox(name));
-        model.addAttribute("name", name);
-        return "index";
+        redirectAttributes.addAttribute("name", name);
+        if (foxService.findFox(name) == null){
+            foxService.addFox(name);
+        }
+        return "redirect:/";
     }
     @GetMapping("/nutritionstore")
-    public String feed (){
+    public String feed (Model model){
+        model.addAttribute("foodlist",foxService.fillFoodList());
+        model.addAttribute("drinklist",foxService.fillDrinkList());
         return "nutritionstore";
     }
 }
--- Orientation/foxclub/src/main/java/com/foxclub/foxclub/Service/FoxService.java
+++ Orientation/foxclub/src/main/java/com/foxclub/foxclub/Service/FoxService.java
@@ -5,14 +5,57 @@ import com.foxclub.foxclub.Repository.FoxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class FoxService {
     FoxRepository foxRepository;
+    private List<Fox> foxes;
+
     @Autowired
     public FoxService(FoxRepository foxRepository) {
         this.foxRepository = foxRepository;
     }
-    public void saveFox (Fox fox) {
+
+    public void saveFox(Fox fox) {
         foxRepository.save(fox);
     }
+
+    public List<String> fillFoodList() {
+        List<String> foodlist = new ArrayList<>();
+        foodlist.add("pizza");
+        foodlist.add("orange");
+        foodlist.add("aranygaluska");
+        return foodlist;
+    }
+
+    public List<String> fillDrinkList() {
+        List<String> drinklist = new ArrayList<>();
+        drinklist.add("water");
+        drinklist.add("beer");
+        drinklist.add("blood");
+        return drinklist;
+    }
+
+    public void addFox(String name) {
+        foxes.add(new Fox(name));
+    }
+
+    public Fox findFox(String name) {
+        Fox fox = foxes.stream().filter(x -> x.getName()
+                .equals(name))
+                .findFirst()
+                .orElse(null);
+        return fox;
+    }
+/*    public Optional<Fox> findFox(String name) {
+        for (Fox fox : foxes) {
+            if (fox.getName().equals(name)) {
+                return Optional.of(fox);
+            }
+        }
+        return Optional.empty();
+    }*/
 }
